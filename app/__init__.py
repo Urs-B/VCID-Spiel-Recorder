b59a135fe0e5cd95faac488455e37f5cc055b4af
Urs-B <97906823+Urs-B@users.noreply.github.com>
2024-03-16 15:39:36 +0100

    v3 - Login und Registrierung
    App funktioniert
    App hat:
    - Skelett
    - Login und Reg funktioniert
    - DB nimmt user
    -----------------------------------------------
    Ab hier Kommentare der Zwischenschritte
    
    Während Login funktionalität
    
    Zwischenschritt, nachdem Requirements für Werkzeug angepasst worden sind.
    Änderungen bis hier:
    - set und check password
    - route mit login und logout
    
    Req nochmals angepasst
    
    musste Version von SQL Alchemy auch anpassen, app lief nicht
    
    DB funktioniert wieder
    
    Basic Login funktioniert
    
    Musste user_id in id umbenennen, da load_user eine id benötigt
    Jetzt funktioniert Login mit weiterleitung auf Index Seite

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
 
 app = Flask(__name__)
@@ -9,5 +10,7 @@ bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -15,12 +16,12 @@ class RegisterForm(FlaskForm):
     password2 = PasswordField('Passwort wiederholen', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Registrieren')
 
-    # def validate_username(self, username):
-    #     user = User.query.filter_by(username=username.data).first()
-    #     if user is not None:
-    #     raise ValidationError('Bitte einen anderen Benutzernamen verwenden.')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Bitte einen anderen Benutzernamen verwenden.')
     
-    # def validate_email(self, email):
-    #     user = User.query.filter_by(email=email.data).first()
-    #     if user is not None:
-    #     raise ValidationError('Bitte eine andere Email Adresse verwenden.')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Bitte eine andere Email Adresse verwenden.')
